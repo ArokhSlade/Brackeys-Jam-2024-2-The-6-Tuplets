7806f6400cdc7e370cf57560fd0768e2f5741c6e
ArokhSlade <139560570+ArokhSlade@users.noreply.github.com>
2024-09-12 20:54:35 +0200

    cleanup: sort files into folders
    also deleted some temp files

--- scenes/general/global_game_state.gd
+++ scenes/general/global_game_state.gd
@@ -3,6 +3,6 @@ extends Node
 var was_game_won = false
 
 #used by main menu and game over menu
-const level_path = "res://scenes/test_level.tscn"
+const level_path = "res://scenes/level assets/level.tscn"
 const game_over_scene_path = "res://scenes/menus/game_over_scene.tscn"
 const main_menuscene_path = "res://scenes/menus/main_menu_scene.tscn"
