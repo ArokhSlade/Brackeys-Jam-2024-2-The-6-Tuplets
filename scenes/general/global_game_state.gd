ef24c21cf05ad9d2a4f781c4cb378a3906e13d5b
ArokhSlade <139560570+ArokhSlade@users.noreply.github.com>
2024-09-13 13:48:54 +0200

    follup to last commit: set resource path to point to new level name
    making it so when start game is pressed, level actually gets loaded still

--- scenes/general/global_game_state.gd
+++ scenes/general/global_game_state.gd
@@ -5,6 +5,6 @@ var was_game_won = false
 
 
 #used by main menu and game over menu
-const level_path = "res://scenes/level assets/level.tscn"
+const level_path = "res://scenes/level assets/level_arokh.tscn"
 const game_over_scene_path = "res://scenes/menus/game_over_scene.tscn"
 const main_menuscene_path = "res://scenes/menus/main_menu_scene.tscn"
