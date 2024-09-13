26501c6c9eb4e099f58f6df0b62bf93c392f3485
ArokhSlade <139560570+ArokhSlade@users.noreply.github.com>
2024-09-13 03:06:02 +0200

    house models, background, post-game timer, storm timer (buggy)
    win game sometimes doesn't work when player picks up material left of spawn point first

--- scenes/general/game_manager.gd
+++ scenes/general/game_manager.gd
@@ -1,11 +1,32 @@
 extends Node
 
 @onready var build_site = $BuildSite
+@onready var past_game_timer = $PastGameTimer
+@onready var storm_timer = $StormTimer
+@export var  time_limit : float
+@onready var timer_label = %TimerLabel
 
-func _process(delta):
-	if build_site.delivered_count == build_site.required_count:
-		win_game()
+func _ready():
+	storm_timer.start(time_limit)
+
+func _process(delta):	
+	if !storm_timer.is_stopped():
+		timer_label.text = "%.2f" % storm_timer.time_left
 
 func win_game():
 	GlobalGameState.was_game_won = true
+	storm_timer.stop()
+	past_game_timer.start()
+	
+func lose_game():
+	GlobalGameState.was_game_won = false
+	past_game_timer.start()
+
+func _on_past_game_timer_timeout():	
 	get_tree().call_deferred("change_scene_to_file",GlobalGameState.game_over_scene_path)
+
+
+func _on_build_site_material_received():
+	if build_site.received_count >= build_site.required_count:
+		if not GlobalGameState.was_game_won:
+			win_game()
--- scenes/level assets/scene_transition_zone.gd
+++ scenes/level assets/scene_transition_zone.gd
@@ -1,9 +1,10 @@
 extends Node3D
 
+signal entered_game_over_zone
+
 @export var scene_path = GlobalGameState.game_over_scene_path
 @export var was_game_won: bool
 
 
 func _on_area_3d_body_entered(_body):
-	GlobalGameState.was_game_won = was_game_won;
-	get_tree().call_deferred("change_scene_to_file",scene_path)	
+	entered_game_over_zone.emit()
