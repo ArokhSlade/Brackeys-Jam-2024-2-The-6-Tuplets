2b775dd0f8afa8ce387991022bf5a23939e338e2
ArokhSlade <139560570+ArokhSlade@users.noreply.github.com>
2024-09-15 06:27:37 +0200

    clean up files

--- scenes/level assets/terrain.gd
+++ scenes/level assets/terrain.gd
@@ -1,4 +1,4 @@
-extends MeshInstance3D
+extends StaticBody3D
 
 const TerrainType = preload("res://scenes/level assets/TerrainType.gd")
 
@@ -15,15 +15,3 @@ const SAND_MATERIAL = preload("res://materials/SAND.tres")
 const SANDSTONE_MATERIAL = preload("res://materials/SANDSTONE.tres")
 const WOOD_MATERIAL = preload("res://materials/WOOD.tres")
 const DIRT_MATERIAL = preload("res://materials/DIRT.tres")
-
-func _ready():	
-	if terrain_type == SANDSTONE:
-		material_override = SANDSTONE_MATERIAL
-	elif terrain_type == SAND:
-		material_override = SAND_MATERIAL
-	elif terrain_type == WOOD:
-		material_override = WOOD_MATERIAL
-	elif terrain_type == DIRT:
-		material_override = DIRT_MATERIAL
-	elif terrain_type == GRAVEL:
-		material_override = GRAVEL_MATERIAL
